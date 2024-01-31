dd8224450f660d65f2159aa59354b4ef793c7bb4
Ruslan <ruslan_savkin_nn@mail.ru>
2024-01-31 14:29:13 +0300

    добавил код сервиса

--- src/main/java/ru/netology/service/Main.java
+++ src/main/java/ru/netology/service/Main.java
@@ -1,8 +1,10 @@
-package ru.netology;
+package ru.netology.service;
+
 public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
         return boundary - amount % boundary;
     }
+}
 }
